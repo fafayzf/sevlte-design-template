4bf577f7ff2787d5a05042c431e9b9b3efd559be
shijianjun <fafayzf@163.com>
2024-06-13 19:48:55 +0800

    style: update dashboard

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -16,4 +16,9 @@
 </script>
 
 <div>dashboard</div>
-<button use:melt={$button} on:click={handleClick}>clear token</button>
+<button
+  use:melt={$button}
+  on:click={handleClick}
+  class="inline-flex w-full h-9 items-center justify-center rounded-md bg-black px-4 py-2 mt-4
+        font-medium leading-none text-magnum-300 shadow-lg hover:opacity-75"
+>clear token</button>
